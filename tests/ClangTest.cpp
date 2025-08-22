2c7aabb93c33f45a065454c7e63c5324af81da94
Hassan Sajjad <hassan.sajjad069@gmail.com>
2025-08-23 04:56:29 +0500

    test passed for module.

--- tests/ClangTest.cpp
+++ tests/ClangTest.cpp
@@ -90,79 +90,53 @@ tl::expected<void, string> Run(const string &command)
 // Creates all the input files (source files + pcm files) that are needed for the test.
 [[nodiscard]] tl::expected<void, string> setupTest()
 {
+    //  A.cpp
+    const string aDotCpp = R"(
+export module A;     // primary module interface unit
 
-    const string mod2 = R"(
-export module mod2;
+export import :B;    // Hello() is visible when importing 'A'.
+import :C;           // WorldImpl() is now visible only for 'A.cpp'.
+// export import :C; // ERROR: Cannot export a module implementation unit.
 
-export void func2()
+// World() is visible by any translation unit importing 'A'.
+export char const* World()
 {
-    // does nothing
+    return WorldImpl();
 }
+)";
+    // A-B.cpp
+    const string aBDotCPP = R"(
+export module A:B; // partition module interface unit
 
-export struct EmptyClass2
-{
-};)";
+// Hello() is visible by any translation unit importing 'A'.
+export char const* Hello() { return "Hello"; }
+)";
 
-    const string mod1 = R"(
-module;
+    // A-C.cpp
+    const string aCDotCPP = R"(
+module A:C; // partition module implementation unit
 
-export module mod1;
-import mod2;)";
+// WorldImpl() is visible by any module unit of 'A' importing ':C'.
+char const* WorldImpl() { return "World"; }
+)";
 
-    const string mod = R"(
-export module mod;
-import mod2;
-
-export void func()
-{
-    EmptyClass2 a;
-    func2();
-    // does nothing
-}
-
-export struct EmptyClass
-{
-};)";
-
-    // main depends on mod and mod1. mod and mod1 both depend on mod2. compiler should only send 2 messages for mod and
-    // mod1. mod2 is received with mod. Build-system won't send the mod2 again with mod1.
-    const string main = R"(
-import mod;
-import mod1;
+    // main.cpp
+    const string mainDotCpp = R"(
+import A;
+import <iostream>;
 
 int main()
 {
-    func();
-    EmptyClass a;
-})";
+    std::cout << Hello() << ' ' << World() << '\n';
+    // WorldImpl(); // ERROR: WorldImpl() is not visible.
+}
+)";
 
-    ofstream("mod.cppm") << mod;
-    ofstream("mod1.cppm") << mod1;
-    ofstream("mod2.cppm") << mod2;
-    ofstream("main.cpp") << main;
+    ofstream("A.cpp") << aDotCpp;
+    ofstream("A-B.cpp") << aBDotCPP;
+    ofstream("A-C.cpp") << aCDotCPP;
+    ofstream("main.cpp") << mainDotCpp;
 
-    if (system(CLANG_CMD
-               R"( -std=c++20 mod2.cppm -c -fmodule-output="mod2 .pcm"  -fmodules-reduced-bmi -o  "mod2 .o")") !=
-        EXIT_SUCCESS)
-    {
-        return tl::unexpected("could not run the first command\n");
-    }
-
-    if (system(
-            CLANG_CMD
-            R"( -std=c++20 mod.cppm -c -fmodule-output="mod .pcm"  -fmodules-reduced-bmi -o  "mod .o"  -fmodule-file=mod2="./mod2 .pcm")") !=
-        EXIT_SUCCESS)
-    {
-        return tl::unexpected("could not run the second command\n");
-    }
-
-    if (system(
-            CLANG_CMD
-            R"( -std=c++20 mod1.cppm -c -fmodule-output="mod1 .pcm"  -fmodules-reduced-bmi -o  "mod1 .o"  -fmodule-file=mod2="./mod2 .pcm")") !=
-        EXIT_SUCCESS)
-    {
-        tl::unexpected("could not run the second command\n");
-    }
     return {};
 }
 
@@ -180,100 +154,173 @@ tl::expected<int, string> runTest()
     string mod1FilePath = current + "mod1 .pcm";
     string mod2FilePath = current + "mod2 .pcm";
 
-    // compiling mod.cppm
-    if (const auto &r = makeIPCManagerBS(std::move(modFilePath)); r)
+    string aCObj = (current_path() / "A-C .o").generic_string();
+    string aCPcm = (current_path() / "A-C .pcm").generic_string();
+    string aBObj = (current_path() / "A-B .o").generic_string();
+    string aBPcm = (current_path() / "A-B .pcm").generic_string();
+    string aObj = (current_path() / "A .o").generic_string();
+
+    // compiling A-C.cpp
     {
-        uint8_t messageCount = 0;
+        const auto &r = makeIPCManagerBS(aCObj);
+        if (!r)
+        {
+            return tl::unexpected("creating manager failed" + r.error() + "\n");
+        }
+
         const IPCManagerBS &manager = *r;
 
-        string objFile = (current_path() / "mod .o").generic_string() + "\"";
-        string compileCommand = CLANG_CMD R"( -std=c++20 -c mod.cppm -noScanIPC -o ")" + objFile;
+        string compileCommand =
+            CLANG_CMD R"( -noScanIPC -std=c++20 -fmodules-reduced-bmi -c -xc++-module A-C.cpp -fmodule-output=")" +
+            aCPcm + "\" -o \"" + aCObj + "\"";
         if (const auto &r2 = Run(compileCommand); !r2)
         {
             return tl::unexpected(r2.error());
         }
 
-        while (true)
+        CTB type;
+        char buffer[320];
+        if (const auto &r2 = manager.receiveMessage(buffer, type); !r2)
         {
-            CTB type;
-            char buffer[320];
-            if (const auto &r2 = manager.receiveMessage(buffer, type); !r2)
-            {
-                string str = r2.error();
-                return tl::unexpected("manager receive message failed" + r2.error() + "\n");
-            }
-
-            switch (type)
-            {
-
-            case CTB::MODULE: {
-                const auto &ctbModule = reinterpret_cast<CTBModule &>(buffer);
-                printMessage(ctbModule, false);
-
-                if (!messageCount)
-                {
-                    ++messageCount;
-
-                    BMIFile mod;
-                    mod.filePath = modFilePath;
-                    mod.fileSize = 0;
-
-                    BMIFile mod2;
-                    mod2.filePath = mod2FilePath;
-                    mod2.fileSize = 0;
-                    ModuleDep mod2Dep;
-                    mod2Dep.file = std::move(mod2);
-                    mod2Dep.logicalName = "mod2";
-
-                    BTCModule b;
-                    b.requested = std::move(mod);
-                    b.deps.emplace_back(std::move(mod2Dep));
-
-                    if (const auto &r2 = manager.sendMessage(b); !r2)
-                    {
-                        return tl::unexpected(r2.error());
-                    }
-                    printMessage(b, true);
-                }
-                else if (messageCount == 1)
-                {
-                    ++messageCount;
-                    BMIFile mod1;
-                    mod1.filePath = mod1FilePath;
-                    mod1.fileSize = 0;
-
-                    BTCModule b;
-                    b.requested = std::move(mod1);
-
-                    if (const auto &r2 = manager.sendMessage(b); !r2)
-                    {
-                        return tl::unexpected(r2.error());
-                    }
-                    printMessage(b, true);
-                }
-            }
-
-            break;
-
-            case CTB::NON_MODULE: {
-            }
-
-            case CTB::LAST_MESSAGE: {
-            }
-
-                return tl::unexpected("Unexpected message received");
-            }
-
-            if (messageCount == 2)
-            {
-                break;
-            }
+            string str = r2.error();
+            return tl::unexpected("manager receive message failed" + r2.error() + "\n");
         }
+
+        if (type != CTB::LAST_MESSAGE)
+        {
+            return tl::unexpected("received message of wrong type");
+        }
+
+        const auto &ctbLastMessage = reinterpret_cast<CTBLastMessage &>(buffer);
+
+        if (ctbLastMessage.logicalName != "A:C")
+        {
+            return tl::unexpected("wrong logical name received while compiling A-C.cpp");
+        }
+        printMessage(ctbLastMessage, false);
     }
-    else
+
+    // compiling A-B.cpp
     {
-        return tl::unexpected("creating manager failed" + r.error() + "\n");
+        const auto &r = makeIPCManagerBS(aBObj);
+        if (!r)
+        {
+            return tl::unexpected("creating manager failed" + r.error() + "\n");
+        }
+
+        const IPCManagerBS &manager = *r;
+
+        string compileCommand =
+            CLANG_CMD R"( -noScanIPC -std=c++20 -fmodules-reduced-bmi -c -xc++-module A-B.cpp -fmodule-output=")" +
+            aBPcm + "\" -o \"" + aBObj + "\"";
+        if (const auto &r2 = Run(compileCommand); !r2)
+        {
+            return tl::unexpected(r2.error());
+        }
+
+        CTB type;
+        char buffer[320];
+        if (const auto &r2 = manager.receiveMessage(buffer, type); !r2)
+        {
+            string str = r2.error();
+            return tl::unexpected("manager receive message failed" + r2.error() + "\n");
+        }
+
+        if (type != CTB::LAST_MESSAGE)
+        {
+            return tl::unexpected("received message of wrong type");
+        }
+
+        const auto &ctbLastMessage = reinterpret_cast<CTBLastMessage &>(buffer);
+
+        if (ctbLastMessage.logicalName != "A:B")
+        {
+            return tl::unexpected("wrong logical name received while compiling A-B.cpp");
+        }
+        printMessage(ctbLastMessage, false);
     }
+
+    // compiling A.cpp
+    {
+        const auto &r = makeIPCManagerBS(aObj);
+        if (!r)
+        {
+            return tl::unexpected("creating manager failed" + r.error() + "\n");
+        }
+
+        const IPCManagerBS &manager = *r;
+
+        string compileCommand = CLANG_CMD R"( -noScanIPC -std=c++20 -c A.cpp -o ")" + aObj + "\"";
+        if (const auto &r2 = Run(compileCommand); !r2)
+        {
+            return tl::unexpected(r2.error());
+        }
+
+        CTB type;
+        char buffer[320];
+        if (const auto &r2 = manager.receiveMessage(buffer, type); !r2)
+        {
+            string str = r2.error();
+            return tl::unexpected("manager receive message failed" + r2.error() + "\n");
+        }
+
+        if (type != CTB::MODULE)
+        {
+            return tl::unexpected("received message of wrong type");
+        }
+
+        const auto &ctbModule = reinterpret_cast<CTBModule &>(buffer);
+
+        if (ctbModule.moduleName != "A:B")
+        {
+            return tl::unexpected("wrong logical name received while compiling A-B.cpp");
+        }
+        printMessage(ctbModule, false);
+
+        BTCModule btcMod;
+        btcMod.requested.filePath = aBPcm;
+        ModuleDep modDep;
+        modDep.file.filePath = aCPcm;
+        modDep.logicalName = "A:C";
+        btcMod.deps.emplace_back(std::move(modDep));
+
+        if (const auto &r2 = manager.sendMessage(std::move(btcMod)); !r2)
+        {
+            string str = r2.error();
+            return tl::unexpected("manager send message failed" + r2.error() + "\n");
+        }
+
+        if (const auto &r2 = manager.receiveMessage(buffer, type); !r2)
+        {
+            string str = r2.error();
+            return tl::unexpected("manager receive message failed" + r2.error() + "\n");
+        }
+
+        if (type != CTB::LAST_MESSAGE)
+        {
+            return tl::unexpected("received message of wrong type");
+        }
+
+        const auto &ctbLastMessage = reinterpret_cast<CTBLastMessage &>(buffer);
+        printMessage(ctbLastMessage, false);
+    }
+
+    BMIFile mod;
+    mod.filePath = modFilePath;
+    mod.fileSize = 0;
+
+    BMIFile mod2;
+    mod2.filePath = mod2FilePath;
+    mod2.fileSize = 0;
+    ModuleDep mod2Dep;
+    mod2Dep.file = std::move(mod2);
+    mod2Dep.logicalName = "mod2";
+
+    BTCModule b;
+    b.requested = std::move(mod);
+    b.deps.emplace_back(std::move(mod2Dep));
+
+    printMessage(b, true);
 
     // compiling main.cpp
     if (const auto &r = makeIPCManagerBS(std::move(mainFilePath)); r)
